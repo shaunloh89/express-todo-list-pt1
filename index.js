6836898ba137867a2de9e059795c0c815ca7c42c
Shaun Loh <loh.shaun89@gmail.com>
2017-07-17 23:40:05 +0800

    handlebars home and todos created

--- index.js
+++ index.js
@@ -1,7 +1,67 @@
 const mongoose = require('mongoose')
 const todosController = require('./controllers/todos_controller')
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const Todo = require('./models/todo')
 
-mongoose.connect('mongodb://localhost/todo-list')
+// connect to database
+const url = 'mongodb://localhost27017/mongo-todo-list'
+
+mongoose.connect(url, {
+  useMongoClient: true
+})
 mongoose.Promise = global.Promise
 
+const app = express()
+
+app.use(express.static('public'))
+
+// to read req.body
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
 // TODO. include express and body-parser, plugin in the todos controller and start listening
+
+app.engine('handlebars', exphbs(
+  {
+    defaultLayout: 'main'
+  }
+))
+app.set('view engine', 'handlebars')
+
+// app.get('/', function (req, res) {
+//   res.render('home')
+// })
+
+
+app.get('/todos', function (req, res) {
+  Todo.find({}, function (err, allTodos) {
+    if (err) throw err
+    res.render('todos/index', {
+      todos: allTodos
+    }
+  )
+  })
+})
+
+// listen to the post request
+// read the form data
+app.post('/todos', function (req, res) {
+  res.send(req.body)
+  var newTodo = new Todo({
+    name: req.body.name,
+    description: req.body.description,
+    completed: req.body.completed
+  })
+  newTodo.save(function (err, newTodo) {
+    console.log('new Todo is saved')
+    res.redirect('/todos')
+  })
+})
+
+const port = 5500
+app.listen(port, function () {
+  console.log('running mongo-todo-list at ' + port)
+})
